9d7edb79dd02e37cdc1fc3d4a728dfeb7807f3b5
Ankit Parikh <aparikh@ibaset.com>
2017-12-07 22:53:39 +0530

    Removed material ui and added grommet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
+import Grommet from 'grommet/components/Grommet';
+import Box from 'grommet/components/Box';
+import Heading from 'grommet/components/Heading';
+import Image from 'grommet/components/Image';
 
 const App = (props) => (
-  <MuiThemeProvider>
-    <AppBar
-      title="iBASEt Box Cricket Tournament - 2017"
-    />
-  </MuiThemeProvider>
+  <Grommet>
+    <Box full align="center">
+      <Image height="50" width="200" src="/images/logo.svg"/>
+      <Heading level={3}>Box Cricket Tournament - 2017</Heading>
+    </Box>
+  </Grommet>
 );
 
 export default App;
