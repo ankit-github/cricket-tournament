7f0fb2c35faa5175290778d3be43b569b1089daa
Parth Patel <parth.orbit@gmail.com>
2017-12-11 10:52:57 +0530

    Gallery changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,10 +3,10 @@ import Box from 'grommet/components/Box';
 import Heading from 'grommet/components/Heading';
 import ImageGallery from 'react-image-gallery';
 import galleryData from '../../data/gallery';
-
+import "react-image-gallery/styles/css/image-gallery.css";
 const Gallery = (props) => (
-  <Box>
-    <Heading level={3}>Gallery</Heading>
+  <Box align="center"> 
+    <Box full="horizontal"><Heading level={3}>Gallery</Heading></Box>
     <ImageGallery items={galleryData.images} showThumbnails={false} />
   </Box>
 );
