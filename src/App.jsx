ea34571c8099f03292aa3d576edd9d1bcdf52e68
Kewal D. Yadav <kewalyadav@gmail.com>
2017-12-14 15:30:50 +0530

    modified home page theme.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Grommet from 'grommet/components/Grommet';
 import Box from 'grommet/components/Box';
 import Heading from 'grommet/components/Heading';
+import Menu from 'grommet/components/Menu';
 import Image from 'grommet/components/Image';
 import { RoutedAnchor } from 'grommet/components/Anchor';
 import Stack from 'grommet/components/Stack';
@@ -17,14 +18,11 @@ import './style.css';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 const App = (props) => (
-  <Grommet>
-    <Box full="horizontal" align="center" tag="header" pad={{horizontal: 'xlarge'}}>
-      <Image height="50" width="200" src="/images/logo.svg"/>
-      <Heading level={3}>Box Cricket Tournament - 2017</Heading>
-    </Box>
-    <Box tag="section" pad={{horizontal: 'xlarge'}}>
-      <Router>
-        <Box>
+  <Grommet className="main-content">
+    <Router>
+      <Box>
+        <Box tag='header' direction='row' fixed={true} className="top-hader">
+          <Image height="80px" src="/images/logo.png"/>
           <Box className="menu-bar" full="horizontal" align="center" direction="row" justify="center">
             <RoutedAnchor path="/">Home</RoutedAnchor>
             <RoutedAnchor path="/rules">Rules</RoutedAnchor>
@@ -35,20 +33,22 @@ const App = (props) => (
             <RoutedAnchor path="/fantasy">Fantasy</RoutedAnchor>
             <RoutedAnchor path="/venue">Venue Details</RoutedAnchor>
           </Box>
-          <Box>
-            <Route path="/" exact component={Home} />
-            <Route path="/rules" exact component={Rules} />
-            <Route path="/schedule" exact component={Schedule} />
-            <Route path="/teams" exact component={ComingSoon} />
-            <Route path="/gallery" exact component={Gallery} />
-            <Route path="/statistics" exact component={ComingSoon} />
-            <Route path="/fantasy" exact component={ComingSoon} />
-            <Route path="/venue" exact component={LocationMap} />
-          </Box>
         </Box>
-      </Router>
-    </Box>
-    <Box tag="footer" align="center" pad={{horizontal: 'xlarge', vertical: 'large'}} alignSelf="end">
+        <Box tag="section" pad={{horizontal: 'xlarge'}} className="content-data">
+            <Box>
+              <Route path="/" exact component={Home} />
+              <Route path="/rules" exact component={Rules} />
+              <Route path="/schedule" exact component={Schedule} />
+              <Route path="/teams" exact component={ComingSoon} />
+              <Route path="/gallery" exact component={Gallery} />
+              <Route path="/statistics" exact component={ComingSoon} />
+              <Route path="/fantasy" exact component={ComingSoon} />
+              <Route path="/venue" exact component={LocationMap} />
+            </Box>
+        </Box>
+      </Box>
+    </Router>
+    <Box tag="footer" align="center" pad={{horizontal: 'xlarge', vertical: 'large'}} alignSelf="end" className="footer-content">
       For internal use only @ iBASEt india
     </Box>
   </Grommet>
