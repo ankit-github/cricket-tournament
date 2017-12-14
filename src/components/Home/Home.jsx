5bc17e8c95d0460137ff4415dc8414bfc470811e
Parth Patel <parth.orbit@gmail.com>
2017-12-14 14:55:37 +0530

    Add responsive layout for Image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,7 +15,7 @@ const Home = (props) => (
       Box Cricket is a variant of and shares many basic concepts with cricket.
       The game is most often played between two teams each consisting of six or eight players.
     </Box>
-    <Box align="center">
+    <Box align="center" responsive>
       <Image size="large" full="vertical" src="/images/Ground.jpeg" />
     </Box>
     <Box>
@@ -23,7 +23,7 @@ const Home = (props) => (
       <p>Small Format, Less Time and Unlimited Fun, Excitement.</p>
       <p>Total 46 players including 10 girls; Formed two groups for men, each group having 3 teams; Two girl teams</p>
     </Box>
-    <Box direction="row" justify="between">
+    <Box direction="row" justify="between" responsive>
        {Object.keys(TeamData).map((team) => 
           <Image key={team} src={TeamData[team].image} {...TeamData[team].mediumSize} />
        )}
