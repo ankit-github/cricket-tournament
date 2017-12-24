a20b1d5907006518a248fcb67d951487f0010587
Ankit Parikh <aparikh@ibaset.com>
2017-12-24 15:46:34 +0530

    Updated data for practice matches

--- src/data/results.js
+++ src/data/results.js
@@ -2,59 +2,59 @@ import schedule from './schedule';
 
 const results = {
   "p1.1": {
-    "royal": {"runs": 100, "wickets": 6, "overs": 5 },
-    "slingers": {"runs": 100, "wickets": 6, "overs": 5},
+    "royal": {"runs": 35, "wickets": 1, "overs": 5 },
+    "slingers": {"runs": 17, "wickets": 6, "overs": 4.3 },
     "result": {
       "winner": "royal",
-      "margin": "XXXX"
+      "margin": "18 runs"
     }
   },
   "p1.2": {
-    "hawk": {"runs": 100, "wickets": 6, "overs": 5 },
-    "lions": {"runs": 100, "wickets": 6, "overs": 5},
+    "hawk": {"runs": 33, "wickets": 4, "overs": 5 },
+    "lions": {"runs": 15, "wickets": 6, "overs": 4 },
     "result": {
       "winner": "hawk",
-      "margin": "XXXX"
+      "margin": "18 runs"
     }
   },
   "p1.3": {
-    "royal": {"runs": 100, "wickets": 6, "overs": 5 },
-    "lions": {"runs": 100, "wickets": 6, "overs": 5},
+    "royal": {"runs": 20, "wickets": 4, "overs": 5 },
+    "lions": {"runs": 30, "wickets": 4, "overs": 5 },
     "result": {
       "winner": "lions",
-      "margin": "XXXX"
+      "margin": "10 runs"
     }
   },
   "p1.4": {
-    "hawk": {"runs": 100, "wickets": 6, "overs": 5 },
-    "slingers": {"runs": 100, "wickets": 6, "overs": 5},
+    "hawk": {"runs": 10, "wickets": 6, "overs": 4 },
+    "slingers": {"runs": 47, "wickets": 1, "overs": 5 },
     "result": {
       "winner": "slingers",
-      "margin": "XXXX"
+      "margin": "37 runs"
     }
   },
   "p2.1": {
-    "guts": {"runs": 100, "wickets": 6, "overs": 5 },
-    "hit": {"runs": 100, "wickets": 6, "overs": 5},
+    "guts": {"runs": 28, "wickets": 3, "overs": 5 },
+    "hit": {"runs": 30, "wickets": 1, "overs": 3.2 },
     "result": {
-      "winner": "slingers",
-      "margin": "XXXX"
+      "winner": "hit",
+      "margin": "4 wickets"
     }
   },
   "p2.2": {
-    "goblin": {"runs": 100, "wickets": 6, "overs": 5 },
-    "musketeers": {"runs": 100, "wickets": 6, "overs": 5},
+    "goblin": {"runs": 27, "wickets": 4, "overs": 5 },
+    "musketeers": {"runs": 36, "wickets": 4, "overs": 5 },
     "result": {
-      "winner": "goblin",
-      "margin": "XXXX"
+      "winner": "musketeers",
+      "margin": "9 runs"
     }
   },
   "p2.3": {
-    "goblin": {"runs": 100, "wickets": 6, "overs": 5 },
-    "musketeers": {"runs": 100, "wickets": 6, "overs": 5},
+    "goblin": {"runs": 25, "wickets": 4, "overs": 5 },
+    "musketeers": {"runs": 17, "wickets": 6, "overs": 4.4 },
     "result": {
-      "winner": "musketeers",
-      "margin": "XXXX"
+      "winner": "goblin",
+      "margin": "8 runs"
     }
   }
 };
